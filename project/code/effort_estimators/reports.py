2d7d1b46c81b2787c529ded14dfa00d0120eb882
Vivek <gvivek19@gmail.com>
2016-11-27 23:23:55 -0500

    pip structure

--- project/code/effort_estimators/reports.py
+++ project/code/effort_estimators/reports.py
@@ -17,22 +17,22 @@ class Reports:
     @staticmethod
     def generate_reports(dataset):
         dataset_name = dataset.split("/")[-1].split(".")[0]
-        op_file = open(config.skott_knott + "_" + dataset_name, "w")
+        op_file = open(config.base_dir + config.skott_knott + "_" + dataset_name, "w")
         if config.ar :
             op_file.write("Absolute Residual Error\n")
-            Reports.analyze_report(config.error_metrics + "_" + dataset_name + "_ar", op_file)
+            Reports.analyze_report(config.base_dir + config.error_metrics + "_" + dataset_name + "_ar", op_file)
         if config.mr :
             op_file.write("\nMagnitude Relative Error\n")
-            Reports.analyze_report(config.error_metrics + "_" + dataset_name + "_mr", op_file)
+            Reports.analyze_report(config.base_dir + config.error_metrics + "_" + dataset_name + "_mr", op_file)
         if config.pred:
             op_file.write("\nPred-25\n")
-            Reports.analyze_report(config.error_metrics + "_" + dataset_name + "_pred", op_file)
+            Reports.analyze_report(config.base_dir + config.error_metrics + "_" + dataset_name + "_pred", op_file)
 
     @staticmethod
     def print_output(dataset):
         dataset_name = dataset.split("/")[-1].split(".")[0]
         print dataset_name
-        fileobject = open(config.skott_knott + "_" + dataset_name, "r")
+        fileobject = open(config.base_dir + config.skott_knott + "_" + dataset_name, "r")
         for line in fileobject :
             print line
         print "\n\n"
---
+++ project/code/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='effort_estimators',
+      version='0.1',
+      description='Effort estimators',
+      url='http://github.com/gvivek19/fss16c/project/code',
+      author='Vivek Arvind',
+      author_email='gvivek19@gmail.com',
+      license='MIT',
+      packages=['effort_estimators'],
+      zip_safe=False)
